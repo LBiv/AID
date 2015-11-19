eec80ecff863af6616c5a56b1b5088c142f52d95
LBiv <ivan@lavabitllc.com>
2015-11-19 18:45:41 -0500

    Added files for error and logging

--- include/common/tokens.h
+++ include/common/tokens.h
@@ -1,9 +1,10 @@
 typedef struct {
-
+    signkey_t *signing;
+    enckey_t *encryption;
+    signkey_t *auxiliary;    
 } provider_token_t;
 
-
 typedef struct {
-
-
+    signkey_t *signing;
+    enckey_t *encryption;
 } user_token_t;
