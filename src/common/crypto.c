7a856b491841a9c15409308053ddde8eb9558bee
LBiv <ivan@lavabitllc.com>
2016-04-09 12:59:27 -0400

    More application-level crypto

--- src/common/crypto.c
+++ src/common/crypto.c
@@ -508,7 +508,76 @@ crypto_kdf(
     unsigned char *key,
     size_t keysize)
 {
+    aid_asymkeys_private_t privkey;
+    aid_asymkeys_public_t pubkey;
+    aid_symmkeys_key_t enckey;
+    int state = 0;
 
+    if (!priv || !pub || !key) {
+        AID_LOG_ERROR(state = AID_ERR_NULL_PTR, NULL);
+        goto out;
+    }
+
+    if (privsize != crypto_asymenc_size_priv()) {
+        AID_LOG_ERROR(state = AID_ERR_BAD_PARAM, NULL);
+        goto out;
+    }
+
+    if (pubsize != crypto_asymenc_size_pub()) {
+        AID_LOG_ERROR(state = AID_ERR_BAD_PARAM, NULL);
+        goto out;
+    }
+
+    if (keysize != crypto_key_size()) {
+        AID_LOG_ERROR(state = AID_ERR_BAD_PARAM, NULL);
+        goto out;
+    }
+
+    if ((state = aid_asymkeys_from_binary_priv(
+        priv,
+        privsize,
+        &privkey)) < 0)
+    {
+        AID_LOG_ERROR(AID_ERR_RETURN, "Failed to deserialize private key");
+        goto out;
+    }
+
+    if ((state = aid_asymkeys_from_binary_pub(
+        pub,
+        pubsize,
+        &pubkey)) < 0)
+    {
+        AID_LOG_ERROR(AID_ERR_RETURN, "Failed to deserialize public key");
+        goto cleanup_privkey;
+    }
+
+    if ((state = aid_kdf_compute(
+        CURRENT_KDF,
+        (aid_asymkeys_private_t const *) &privkey,
+        (aid_asymkeys_public_t const *) &pubkey,
+        &enckey)) < 0)
+    {
+        AID_LOG_ERROR(AID_ERR_RETURN, "Failed to compute encryption key via kdf");
+        goto cleanup_pubkey;
+    }
+
+    if ((state = aid_symmkeys_to_binary(
+        (aid_symmkeys_key_t const *) &enckey,
+        key,
+        keysize)) < 0)
+    {
+        AID_LOG_ERROR(AID_ERR_RETURN, "Failed to serialize encryption key");
+        goto cleanup_enckey;
+    }
+
+cleanup_enckey:
+    aid_symmkeys_cleanup(&enckey);
+cleanup_pubkey:
+    aid_asymkeys_cleanup_pub(&pubkey);
+cleanup_privkey:
+    aid_asymkeys_cleanup_priv(&privkey);
+out:
+    return state;
 }
 
 int
@@ -524,7 +593,93 @@ crypto_asym_encrypt(
     unsigned char const *pub,
     size_t pubsize)
 {
+    aid_asymkeys_private_t privkey;
+    aid_asymkeys_public_t pubkey;
+    aid_symkeys_key_t enckey;    
+    int state = 0;
 
+    if (!data || !cipherbuf || !iv ||  !priv || !pub) {
+        AID_LOG_ERROR(state = AID_ERR_NULL_PTR, NULL);
+        goto out;
+    }
+
+    if (!dsize) {
+        AID_LOG_ERROR(state = AID_ERR_BAD_PARAM, NULL);
+        goto out;
+    }
+
+    if (cipherlen != crypto_cipherlen(dsize)) {
+        AID_LOG_ERROR(state = AID_ERR_BAD_PARAM, NULL);
+        goto out;
+    }
+
+    if (ivlen != crypto_iv_size()) {
+        AID_LOG_ERROR(state = AID_ERR_BAD_PARAM, NULL);
+        goto out;
+    }
+
+    if (privsize != crypto_asymenc_size_priv()) {
+        AID_LOG_ERROR(state = AID_ERR_BAD_PARAM, NULL);
+        goto out;
+    }
+
+    if (pubsize != crypto_asymenc_size_pub()) {
+        AID_LOG_ERROR(state = AID_ERR_BAD_PARAM, NULL);
+        goto out;
+    }
+
+    if ((state = aid_asymkeys_from_binary_priv(
+        priv,
+        privsize,
+        &privkey)) < 0)
+    {
+        AID_LOG_ERROR(AID_ERR_RETURN, "Failed to deserialize private key");
+        goto out;
+    }
+
+    if ((state = aid_asymkeys_from_binary_pub(
+        pub,
+        pubsize,
+        &pubkey)) < 0)
+    {
+        AID_LOG_ERROR(AID_ERR_RETURN, "Failed to deserialize public key");
+        goto cleanup_privkey;
+    }
+
+    if ((state = aid_kdf_compute(
+        CURRENT_KDF,
+        (aid_asymkeys_private_t const *) &privkey,
+        (aid_asymkeys_public_t const *) &pubkey,
+        &enckey)) < 0)
+    {
+        AID_LOG_ERROR(AID_ERR_RETURN, "Failed to compute encryption key via kdf");
+        goto cleanup_pubkey;
+    }
+
+    cipherbuf[0] = (unsigned char) CURRENT_SYMMCRYPT;
+
+    if ((state = aid_symmcrypt_encrypt(
+        CURRENT_SYMMCRYPT,
+        data,
+        dsize,
+        cipherbuf + 1,
+        cipherlen - 1,
+        iv,
+        ivsize,
+        (aid_symmkeys_t const *) &enckey)) < 0)
+    {
+        AID_LOG_ERROR(AID_ERR_RETURN, "Failed to encrypt data");
+        goto cleanup_enckey;
+    }
+
+cleanup_enckey:
+    aid_symmkeys_cleanup(&enckey);
+cleanup_pubkey:
+    aid_asymkeys_cleanup_pub(&pubkey);
+cleanup_privkey:
+    aid_asymkeys_cleanup_priv(&privkey);
+out:
+    return state;
 }
 
 int
@@ -540,33 +695,237 @@ crypto_asym_decrypt(
     unsigned char const *pub,
     size_t pubsize)
 {
+    aid_asymkeys_private_t privkey;
+    aid_asymkeys_public_t pubkey;
+    aid_symkeys_key_t enckey;    
+    int state = 0;
+
+    if (!data || !plainbuf || !iv ||  !priv || !pub) {
+        AID_LOG_ERROR(state = AID_ERR_NULL_PTR, NULL);
+        goto out;
+    }
+
+    if (dsize <= 1) {
+        AID_LOG_ERROR(state = AID_ERR_BAD_PARAM, NULL);
+        goto out;
+    }
+
+    if (plainlen != crypto_plainlen(dsize)) {
+        AID_LOG_ERROR(state = AID_ERR_BAD_PARAM, NULL);
+        goto out;
+    }
+
+    if (ivlen != crypto_iv_size()) {
+        AID_LOG_ERROR(state = AID_ERR_BAD_PARAM, NULL);
+        goto out;
+    }
+
+    if (privsize != crypto_asymenc_size_priv()) {
+        AID_LOG_ERROR(state = AID_ERR_BAD_PARAM, NULL);
+        goto out;
+    }
+
+    if (pubsize != crypto_asymenc_size_pub()) {
+        AID_LOG_ERROR(state = AID_ERR_BAD_PARAM, NULL);
+        goto out;
+    }
+
+    if ((state = aid_asymkeys_from_binary_priv(
+        priv,
+        privsize,
+        &privkey)) < 0)
+    {
+        AID_LOG_ERROR(AID_ERR_RETURN, "Failed to deserialize private key");
+        goto out;
+    }
+
+    if ((state = aid_asymkeys_from_binary_pub(
+        pub,
+        pubsize,
+        &pubkey)) < 0)
+    {
+        AID_LOG_ERROR(AID_ERR_RETURN, "Failed to deserialize public key");
+        goto cleanup_privkey;
+    }
+
+    if ((state = aid_kdf_compute(
+        CURRENT_KDF,
+        (aid_asymkeys_private_t const *) &privkey,
+        (aid_asymkeys_public_t const *) &pubkey,
+        &enckey)) < 0)
+    {
+        AID_LOG_ERROR(AID_ERR_RETURN, "Failed to compute encryption key via kdf");
+        goto cleanup_pubkey;
+    }
+
+    if ((state = aid_symmcrypt_decrypt(
+        (aid_symmcrypt_t) data[0],
+        data,
+        dsize,
+        plainbuf,
+        plainlen,
+        iv,
+        ivsize,
+        (aid_symmkeys_t const *) &enckey)) < 0)
+    {
+        AID_LOG_ERROR(AID_ERR_RETURN, "Failed to decrypt data");
+        goto cleanup_enckey;
+    }
+
+cleanup_enckey:
+    aid_symmkeys_cleanup(&enckey);
+cleanup_pubkey:
+    aid_asymkeys_cleanup_pub(&pubkey);
+cleanup_privkey:
+    aid_asymkeys_cleanup_priv(&privkey);
+out:
+    return state;
 
 }
 
 
 /** asymmetric signing crypto */
 size_t
-crypto_asymsign_size_sig(void);
+crypto_asymsign_size_sig(void) {
+    return aid_asymsign_index(CURRENT_ASYMSIGN)->sig_size;
+}
+
 
 size_t
-crypto_asymsign_size_priv(void);
+crypto_asymsign_size_priv(void) {
+    return aid_asymsign_index(CURRENT_ASYMSIGN)->priv_size;
+}
+
 
 size_t
-crypto_asymsign_size_pub(void);
+crypto_asymsign_size_pub(void) {
+    return aid_asymsign_index(CURRENT_ASYMSIGN)->pub_size;
+}
+
 
 int
 crypto_asymsign_generate(
     unsigned char *priv,
     size_t privsize,
     unsigned char *pub,
-    size_t pubsize);
+    size_t pubsize)
+{
+    aid_asymkeys_index_t const *index;
+    int state = 0;
+    aid_asymkeys_priv_t privkey;
+    aid_asymkeys_pub_t pubkey;
+
+    if (!priv || !pub) {
+        AID_LOG_ERROR(state = AID_ERR_NULL_PTR, NULL);
+        goto out;
+    }
+
+    if (privsize != crypto_asymenc_size_priv()) {
+        AID_LOG_ERROR(state = AID_ERR_BAD_PARAM, NULL);
+        goto out;
+    }
+
+    if (pubsize != crypto_asymenc_size_pub()) {
+        AID_LOG_ERROR(state = AID_ERR_BAD_PARAM, NULL);
+        goto out;
+    }
+
+    if ((state = aid_asymkeys_generate(
+        CURRENT_ASYM_SIGNKEY,
+        &crypto_rand,
+        rng_ctx,
+        &privkey,
+        &pubkey)) < 0)
+    {
+        AID_LOG_ERROR(AID_ERR_RETURN, "Failed to encryption keypair");
+        goto out;
+    }
+
+    if ((state = aid_asymkeys_to_binary_priv(
+        (aid_asymkeys_private_t const *) privkey,
+        priv,
+        privsize)) < 0)
+    {
+        AID_LOG_ERROR(AID_ERR_RETURN, "Failed to deserialize private key");
+        goto cleanup_keys;
+    }
+
+    if ((state = aid_asymkeys_to_binary_pub(
+        (aid_asymkeys_public_t const *) pubkey,
+        pub,
+        pubsize)) < 0)
+    {
+        AID_LOG_ERROR(AID_ERR_RETURN, "Failed to deserialize public key");
+        goto cleanup_keys;
+    }
+
+cleanup_keys:
+    aid_asymkeys_cleanup_priv(&priv);
+    aid_asymkeys_cleanup_pub(&pub);
+out:
+    return state;
+}
+
 
 int
 crypto_asymsign_public(
     unsigned char const *priv,
     size_t privsize,
     unsigned char *pub,
-    size_t pubsize);
+    size_t pubsize)
+{
+    aid_asymkeys_priv_t privkey;
+    aid_asymkeys_pub_t pubkey;
+    int state;
+
+    if (!priv || !pub) {
+        AID_LOG_ERROR(state = AID_ERR_NULL_PTR, NULL);
+        goto out;
+    }
+
+    if (privsize != crypto_asymenc_size_priv()) {
+        AID_LOG_ERROR(state = AID_ERR_BAD_PARAM, NULL);
+        goto out;
+    }
+
+    if (pubsize != crypto_asymenc_size_pub()) {
+        AID_LOG_ERROR(state = AID_ERR_BAD_PARAM, NULL);
+        goto out;
+    }
+
+    if ((state = aid_asymkeys_from_binary_priv(
+        priv,
+        privsize,
+        &aid_asymkeys_priv_t)) < 0)
+    {
+        AID_LOG_ERROR(AID_ERR_RETURN, "Failed to deserialize private key");
+        goto out;
+    }
+
+    if ((state = aid_asymkeys_public(
+        (aid_asymkeys_priv_t const *) &privkey,
+        &pubkey)) < 0)
+    {
+        AID_LOG_ERROR(AID_ERR_RETURN, "Failed to calculate public key");
+        goto cleanup_priv;
+    }
+
+    if ((state = aid_asymkeys_to_binary_pub(
+        (aid_asymkeys_pub_t const *) &pubkey,
+        pub,
+        pubsize)) < 0)
+    {
+        AID_LOG_ERROR(AID_ERR_RETURN, "Failed to serialize public key");
+    }
+
+    aid_asymkeys_cleanup_pub(&pubkey);
+
+cleanup_priv:
+    aid_asymkeys_cleanup_priv(&privkey);
+out:
+    return state;
+}
+
 
 int
 crypto_sign(
@@ -575,7 +934,10 @@ crypto_sign(
     unsigned char *sigbuf,
     size_t sigsize,
     unsigned char const *priv,
-    size_t privsize);
+    size_t privsize)
+{
+
+}
 
 // 0 verified successfully
 // 1 failed to verify
